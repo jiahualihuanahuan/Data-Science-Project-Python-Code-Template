b075d6c9b9d17591b7f20beeb9ea63bf704397f2
Jiahua Li <jiahuali1991@gmail.com>
2020-09-18 17:57:00 -0400

    Create import data.py

---
+++ import data.py
@@ -0,0 +1,16 @@
+# pandas library
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+import pandas as pd
+df = pd.read_csv('file_path', index_col=0, header=0)
+
+# numpy library
+# https://numpy.org/doc/stable/reference/generated/numpy.genfromtxt.html
+from numpy import genfromtxt
+my_data = genfromtxt('my_file.csv', delimiter=',')
+
+# csv library
+import csv
+with open('winequality-red.csv', 'r') as f:
+    wines = list(csv.reader(f, delimiter=';'))
+
+
