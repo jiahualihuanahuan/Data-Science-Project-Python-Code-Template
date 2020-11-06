4b246e46d3e78d8757e148a2af602e6ceb1c8b0f
jiahualihuanahuan <jiahuali1991@gmail.com>
2020-11-06 13:55:15 -0500

    update

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -3,14 +3,18 @@ Gets to 99.25% test accuracy after 12 epochs
 (there is still a lot of margin for parameter tuning).
 16 seconds per epoch on a GRID K520 GPU.
 '''
-
 from __future__ import print_function
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+
+
 
 batch_size = 128
 num_classes = 10
