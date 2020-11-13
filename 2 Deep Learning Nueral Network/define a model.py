ef37c6f5c72c15997dcd9728310e64abe6c4b884
jiahualihuanahuan <jiahuali1991@gmail.com>
2020-11-13 11:06:40 -0500

    Update define a model.py

--- 2 Deep Learning Nueral Network/define a model.py
+++ 2 Deep Learning Nueral Network/define a model.py
@@ -47,7 +47,7 @@ x = tf.ones((3, 3))
 y = layer3(layer2(layer1(x)))
 
 #---------------------------------------
-# Layers needs input(input size) to be able to create their weights
+# 4 Layers needs input(input size) to be able to create their weights
 layer = layers.Dense(3)
 layer.weights  # Empty
 # Call layer on a test input
@@ -55,6 +55,7 @@ x = tf.ones((1, 4))
 y = layer(x)
 layer.weights  # Now it has weights, of shape (4, 3) and (3,)
 #---------------------------------------
+# 5 Specify input shape
 # Specify input shape by Input(shape=(...))
 model = keras.Sequential()
 model.add(keras.Input(shape=(4,)))
@@ -62,8 +63,42 @@ model.add(layers.Dense(2, activation="relu"))
 
 model.summary()
 
+# Specify input shape by input_shape=(...)
+model = keras.Sequential()
+model.add(layers.Dense(2, activation="relu", input_shape=(4,)))
+
+model.summary()
+
 #---------------------------------------
-# Debugging model build
+# 6 A common debugging workflow: add() + summary()
+model = keras.Sequential()
+model.add(keras.Input(shape=(250, 250, 3)))  # 250x250 RGB images
+model.add(layers.Conv2D(32, 5, strides=2, activation="relu"))
+model.add(layers.Conv2D(32, 3, activation="relu"))
+model.add(layers.MaxPooling2D(3))
+
+# Can you guess what the current output shape is at this point? Probably not.
+# Let's just print it:
+model.summary()
+
+# The answer was: (40, 40, 32), so we can keep downsampling...
+
+model.add(layers.Conv2D(32, 3, activation="relu"))
+model.add(layers.Conv2D(32, 3, activation="relu"))
+model.add(layers.MaxPooling2D(3))
+model.add(layers.Conv2D(32, 3, activation="relu"))
+model.add(layers.Conv2D(32, 3, activation="relu"))
+model.add(layers.MaxPooling2D(2))
+
+# And now?
+model.summary()
+
+# Now that we have 4x4 feature maps, time to apply global max pooling.
+model.add(layers.GlobalMaxPooling2D())
+
+# Finally, we add a classification layer.
+model.add(layers.Dense(10))
+
 #---------------------------------------
 #---------------------------------------
 #---------------------------------------
